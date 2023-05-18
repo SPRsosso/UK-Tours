97a1e0d6b09dd64dec2cf30bddf942830ce1f08c
sosso <cypossosmu51c@gmail.com>
2023-05-18 22:10:50 +0200

    Added sliding animation for offers

--- JS/offers.js
+++ JS/offers.js
@@ -1,68 +1,28 @@
-let tmp = 1;
-let tab = [];
-
 function changeOffer(num) 
 {
-
-    // var offers = document.querySelectorAll('.offer');
-    // offers.forEach(function (offer) {
-    //     offer.classList.remove('rev-slide-animation');
-    // });
-
-    // var currentOffer = document.querySelector('[data="' + getOfferData(tmp) + '"]');
-    // currentOffer.classList.add('rev-slide-animation ');
-
-    let x = tab[tab.length-2]
-    var offers = document.querySelectorAll('.offer');
-    offers.forEach(function (offer) {
-        offer.classList.remove('rev-slide-animation')
-        offer.classList.remove('slide-animation');
-    });
-
-    // var currentOffer = document.querySelector('[data="' + getOfferData(x) + '"]');
-    // currentOffer.classList.add('rev-slide-animation')
-
-    // var selectedOffers = document.querySelector('[data="' + getOfferData(x) + '"]');
-    // selectedOffers.classList.add('rev-slide-animation');
-
-    var selectedOffers = document.querySelector('[data="' + getOfferData(num) + '"]');
-    selectedOffers.classList.add('slide-animation');
-
+    let articles = document.querySelectorAll("#offers article");
+    let count = 0;
     switch (num) {
         case 1:
-            document.querySelector("[data='one-star']").style.display = "block"
-            document.querySelector("[data='two-star']").style.display = "none"
-            document.querySelector("[data='three-star']").style.display = "none"
-            tmp = 1
-            tab.push(tmp);
+            count = 0;
+            articles.forEach(( article ) => {
+                article.style.left = count + "vw";
+                count += 100;
+            });
             break;
         case 2:
-            document.querySelector("[data='one-star']").style.display = "none"
-            document.querySelector("[data='two-star']").style.display = "block"
-            document.querySelector("[data='three-star']").style.display = "none"
-            tmp = 2
-            tab.push(tmp);
+            count = -100;
+            articles.forEach(( article ) => {
+                article.style.left = count + "vw";
+                count += 100;
+            });
             break;
         case 3:
-            document.querySelector("[data='one-star']").style.display = "none"
-            document.querySelector("[data='two-star']").style.display = "none"
-            document.querySelector("[data='three-star']").style.display = "block"
-            tmp = 3
-            tab.push(tmp);
+            count = -200;
+            articles.forEach(( article ) => {
+                article.style.left = count + "vw";
+                count += 100;
+            });
             break;
     }
 }
-
-function getOfferData(num) 
-{
-    switch (num) {
-        case 1:
-            return 'one-star';
-        case 2:
-            return 'two-star';
-        case 3:
-            return 'three-star';
-    }
-}
-
-changeOffer(1);
